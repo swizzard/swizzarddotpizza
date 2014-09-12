00015484c5d26eb95832d71b53efbc59b0fb1f07
Sam Raker <sam.raker@gmail.com>
2014-09-12 02:19:31 -0400

    ch-ch-ch-changes

--- samrakerdotcom/urls.py
+++ samrakerdotcom/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 #from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from views import main, load_cv, load_etc
+from views import main, load_cv, load_etc, load_vst
 # VERSION = 'production'
 # handler404 = 'ffy.views.error_404'
 # handler500 = 'ffy.views.error_500'
@@ -13,22 +13,16 @@ urlpatterns = patterns('',
     url(r'^$', main),
     url(r'^load_cv/$', load_cv),
     url(r'^load_etc/$', load_etc),
+    url(r'^load_vst/$', load_vst)
     url(r'^cv/$', main),
     url(r'^about/$', main),
     url(r'^etc/$', main),
     url(r'^survey/$', main),
+    url(r'^vst/$', main),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^comments/', include('django.contrib.comments.urls')),
-    url(r'hash/', include('hash_to_hash.urls')),
-    url(r'ffy/', include('ffy.urls')),
+    # url(r'hash/', include('hash_to_hash.urls')),
+    # url(r'ffy/', include('ffy.urls')),
     #url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     )
 
 urlpatterns += staticfiles_urlpatterns()
-
-# urlpatterns += patterns('django.contrib.flatpages.views',
-# url(r'^$', 'flatpage', {'url':'/test/'}, name='test'),
-# url(r'^cv/$', 'flatpage', {'url':'/cv/'}, name='cv'),
-# url(r'^else/$', 'flatpage', {'url':'/else/'}, name='else'),
-# )
-
