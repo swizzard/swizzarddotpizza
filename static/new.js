916fdfccf519e1dc54c44eb2541733f57b02e93f
Sam Raker <sam.raker@gmail.com>
2014-09-16 19:43:34 -0400

    closing vst when other headers get clicked, adding click event to h1 to close all the things

--- static/new.js
+++ static/new.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         $(".mail").html('<a href="mailto:' + ['sa', 'm.r', 'ak', 'er@', 'gm', 'ai', 'l.c', 'om'].join('') + '">Sam Raker</a>');
     };
     do_mail();
-        close_about = function () {
+    close_about = function () {
         $("#about-parent").html("<h2 id='about'>About</h2>");
         return false;
     };
@@ -13,46 +13,56 @@ $(document).ready(function () {
         $("#cv-parent").html("<h2 id='cv'>CV</h2>");
         return false;
     };
-
     close_survey = function () {
         $("#survey-parent").html("<h2>Survey</h2>");
+        return false;
     };
     close_vst = function () {
-      $("#vst-parent").html("<h2>Very Silly Things&reg;</h2>");
+      $("#vst-parent").html("<h2 id='vst'>Very Silly Things&reg;</h2>");
+      return false;
     };
     load_about = function () {
         $("#about-parent").load("/load_etc/");
-        $("#cv-parent").html("<h2 id='cv'>CV</h2>");
-        $("#survey-parent").html("<h2 id='survey'>Survey</h2>");
+        close_vst();
+        // close_survey();
+        close_vst();
         $("#about-parent").on('click', '.grey', close_about);
         return false;
     };
     load_cv = function () {
         $("#cv-parent").load("/load_cv/");
-        $("#about-parent").html("<h2 id='about'>About</h2>");
-        $("#survey-parent").html("<h2 id='survey'>Survey</h2>");
+        close_about();
+        // close_survey();
+        close_vst();
         $("#cv-parent").on('click', '.grey', close_cv);
         return false;
     };
     load_survey = function () {
         $("#survey-parent").load("/hash_to_hash/");
-        $("#about-parent").html("<h2 id='about'>About</h2>");
-        $("#cv-parent").html("<h2 id='cv'>CV</h2>");
+        close_about();
+        close_cv();
+        close_vst();
         $("#survey-parent").on('click', '.grey', close_survey);
         return false;
     };
     load_vst = function () {
       $("#vst-parent").load("/load_vst/");
-      $("#about-parent").html("<h2 id='about'>About</h2>");
-      $("#cv-parent").html("<h2 id='cv'>CV</h2>");
+      close_about();
+      close_cv();
+      // close_survey();
       $("#vst-parent").on('click', '.grey', close_vst);
     };
+
 
     $("#about-parent").on('click', 'h2', load_about);
     $("#cv-parent").on('click', 'h2', load_cv);
     $("#survey-parent").on('click', 'h2', load_survey);
     $("#vst-parent").on('click', 'h2', load_vst);
-
+    $("#main-title").on('click', 'h1', function() {
+      close_about();
+      close_cv();
+      // close_survey();
+      close_vst();});
 
     switch(window.location.pathname) {
       case "/about/":
